8321e087c174390600ac29830a5e79434744589b
zacheratk <182299683+zacheratk@users.noreply.github.com>
2025-07-20 17:03:53 -0600

    Create basic about page with routing Added dependency react-router-dom for page navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,56 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Dashboard from './Components/Dashboard';
-import LoginPrompt from './Components/LoginPrompt';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import LoginPrompt from "./Components/LoginPrompt";
+import About from "./Routes/About";
 
 const App = () => {
   const [accessToken, setAccessToken] = useState(null);
 
   useEffect(() => {
     // If the user previously logged in, the token should be in sessionStorage
-    const token = sessionStorage.getItem('accessToken');
+    const token = sessionStorage.getItem("accessToken");
     if (token) {
       setAccessToken(token);
     }
-  }, [])
+  }, []);
 
   return (
-  <>
-    <nav className="sidebar glass-panel">
-      <ul>
-        <li>Dashboard</li>
-        <li>About</li>
-      </ul>
-    </nav>
-    <div className='content'>
-      {/* Header that will remain persistent regardless of page */}
-      <header>
-        <h1><span className='accent'>Rhythm</span> <span>Radar</span></h1>
-      </header>
-      {accessToken ? (
-        <Dashboard accessToken={accessToken}/>
-      ) : (
-        <LoginPrompt setAccessToken={setAccessToken} />
-      )}
-    </div>
-  </>
-  )
-}
+    <>
+      <nav className="sidebar glass-panel">
+        <ul>
+          <li>
+            <NavLink to="/">Dashboard</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="content">
+        {/* Header that will remain persistent regardless of page */}
+        <header>
+          <h1>
+            <span className="accent">Rhythm</span> <span>Radar</span>
+          </h1>
+        </header>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              accessToken ? (
+                <Dashboard accessToken={accessToken} />
+              ) : (
+                <LoginPrompt setAccessToken={setAccessToken} />
+              )
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/Routes/About.jsx
@@ -0,0 +1,17 @@
+import "./About.css";
+
+const About = () => {
+  return (
+    <article className="glass-panel">
+      <p>
+        Welcome to Rhythm Radar! View the top trending tracks, and find new
+        ones!
+      </p>
+      <p>
+        In order to use this app, you need to log into your spotify account.
+      </p>
+    </article>
+  );
+};
+
+export default About;
